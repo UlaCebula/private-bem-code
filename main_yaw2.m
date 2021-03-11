b148f78a5da48372826d8e854d10d095bfe157e5
ltyap <litoong.yap@gmail.com>
2021-03-11 15:25:46 +0100

    modified velocity equations

--- main_yaw2.m
+++ main_yaw2.m
@@ -13,7 +13,7 @@ zeroyawresults = load('zeroyawdata.mat');
 a_zeroyaw = zeroyawresults.SectionResults(:,2);
 aprime_zeroyaw = zeroyawresults.SectionResults(:,3);
 %%%
-yaw = deg2rad(0); 
+yaw = deg2rad(15); 
 dPsi = 1;%[deg]
 AzimuthAngle = deg2rad([0:dPsi:360]');
 %%%
@@ -31,19 +31,19 @@ ylabel('C_{d}');
 %%
 % SectionResults = zeros(length(prop.r_R)-1,8);
 for i=1:length(prop.r_R)-1
-    for j=1%for each angle
+    for j=1:length(AzimuthAngle)%for each angle
         prop.sectionchord = chord_distribution(0.5*(prop.r_R(i)+prop.r_R(i+1)), prop.R, propellertype);%non-dimensional
         prop.sectionpitch = pitch_distribution(0.5*(prop.r_R(i)+prop.r_R(i+1)),prop.collective_blade_twist, propellertype);% [deg]        
         [SectionResults(i,:), a(i,j), aprime(i,j), alpha(i,j), inflowangle(i,j)] = SolveSection(i, polar, prop, air, oper, AzimuthAngle(j), dPsi, yaw);
     end
 end
 
-figure;
-plot(SectionResults(:,1), a,'k--');
-hold on
-plot(SectionResults(:,1),a_zeroyaw,'k');
-plot(SectionResults(:,1),aprime,'b--');
-plot(SectionResults(:,1),aprime_zeroyaw,'b');
+% figure;
+% plot(SectionResults(:,1), a,'k--');
+% hold on
+% plot(SectionResults(:,1),a_zeroyaw,'k');
+% plot(SectionResults(:,1),aprime,'b--');
+% plot(SectionResults(:,1),aprime_zeroyaw,'b');
 %% Functions
 function [ftip, froot, ftotal] = PrandtlTipRootCorrection(prop, SectionRadius, InflowAngle)
     r_R_Section = SectionRadius/prop.R; %non-dimensional
@@ -88,7 +88,7 @@ function [Results, a_new, aprime_new, alpha, inflowangle] = SolveSection(index, 
     %calculate a and aprime for each section at a constant radius for each azimuth angle 
     for i=1:N
         %produce array of local AoA, Inflow angles, cl and cd
-        [inflowangle, alpha, cx, cy] = SectionInflowAngleCalc(oper, a, aprime, SectionRadius, polar, prop);
+        [inflowangle, alpha, cx, cy] = SectionInflowAngleCalc(oper, a, aprime, SectionRadius, polar, prop, yaw, AzimuthAngle);
         SectionRotorSolidity = (prop.Nblades*prop.sectionchord*prop.R)/(2*pi*SectionRadius);
         [~, ~, Prandtl] = PrandtlTipRootCorrection(prop, SectionRadius, inflowangle);
         CTsection = CTCalc(SectionRotorSolidity, cx, a, inflowangle); 
@@ -107,9 +107,9 @@ function [Results, a_new, aprime_new, alpha, inflowangle] = SolveSection(index, 
     end 
     Results = [SectionRadius/prop.R, i];
 end
-function [phi, alpha, cx, cy] = SectionInflowAngleCalc(oper, a, aprime, SectionRadius, polar, prop)
-    U_axial = oper.U_inf*(1-a);
-    U_tan = oper.omega*SectionRadius*(1+aprime);
+function [phi, alpha, cx, cy] = SectionInflowAngleCalc(oper, a, aprime, SectionRadius, polar, prop, yaw, AzimuthAngle)
+    U_axial = oper.U_inf*(cos(yaw))*(1-a);
+    U_tan = (oper.U_inf*(-sin(yaw)*cos(AzimuthAngle))+(oper.omega*SectionRadius))*(1+aprime);
     phi = atan2(U_axial,U_tan);
     alpha = rad2deg(phi) - prop.sectionpitch;    
     cl = interp1(polar.alpha, polar.Cl, alpha,'linear', 'extrap');
@@ -145,6 +145,6 @@ function wakeskewangle = WakeSkewAngleCalc(yaw, a)
 end
 function a_skew = SkewedWakeCorrection(a,SectionRadius,prop,chi,AzimuthAngle)
     r_R = SectionRadius./prop.R;
-    temp = (15*pi/32).*r_R.*tan(chi/2).*cos(AzimuthAngle);
+    temp = (15*pi/64).*r_R.*tan(chi/2).*cos(AzimuthAngle);
     a_skew = a.*(1+temp);
 end
