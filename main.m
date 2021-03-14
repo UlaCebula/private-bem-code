cc5390fd9bc55685cbc5669be1a89b5e509d88dd
ltyap <litoong.yap@gmail.com>
2021-03-14 04:01:26 +0100

    revert changes

--- main.m
+++ main.m
@@ -7,10 +7,7 @@ spacing = 'constant';
 type = 'WT';
 [polar, prop, oper, air, propellertype] = param(spacing, type);
 
-zeroyawresults = load('zeroyawdata.mat');
-radialdist_zeroyaw = zeroyawresults.SectionResults(:,1);
-a_zeroyaw = zeroyawresults.SectionResults(:,2);
-aprime_zeroyaw = zeroyawresults.SectionResults(:,3);
+
 % figure
 % subplot(1,2,1);
 % plot(polar.alpha,polar.Cl);
@@ -28,10 +25,7 @@ for i=1:length(prop.r_R)-1
     prop.sectionpitch = pitch_distribution(0.5*(prop.r_R(i)+prop.r_R(i+1)),prop.collective_blade_twist, propellertype);% [deg]
     SectionResults(i,:) = SolveSection(i, polar, prop, air, oper);
 end
-
-plot(SectionResults(:,1), SectionResults(:,2),'k');
-hold on
-plot(radialdist_zeroyaw, a_zeroyaw,'b');
+% 
 dT = SectionResults(:,4).*prop.Nblades.*(prop.dr.*prop.R);
 % CT = sum(dT./(0.5*air.density*(oper.U_inf^2)*(pi*prop.R^2)))
 CT = sum(dT./(0.5*(oper.U_inf^2)*(pi*prop.R^2)))
@@ -52,40 +46,19 @@ function a = AxialInductionFactor(CT)
         btm = 4*sqrt(CT1)-4;
         a = 1+(top/btm);
     end
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
-%     if (CT>0.96*F)%(CT, F, sigma, cx, phi)
-%         temp1 = CT.*(50-36.*F)+12.*F.*(3.*F-4);
-%         top = 18.*F-20-3.*sqrt(temp1);
-%         btm = 36.*F-50;
-%         a = top./btm;
-%     else
-%         btm = sigma.*cx;
-%         top = 4.*F.*(sin(phi)).^2;        
-%         a = (1+(top./btm)).^-1;
-%     end
-%     
-    
-    
 end
-function aprime = AzimInductionFactor(Fazim, prop, SectionRadius, oper, a, air)
-    top = Fazim * prop.Nblades;%(Fazim, prop, SectionRadius, oper, a, air)   
+function aprime = AzimInductionFactor(Fazim, prop, SectionRadius, oper, a, air)    
+    top = Fazim * prop.Nblades;
     btm = 2*air.density*pi*oper.U_inf*(1-a)*oper.omega*2*SectionRadius^2;    
     aprime = top/btm;
-    
-    %%%%%%%%%%%
-%     top = 4.*F.*sin(phi).*cos(phi);(F, phi, sigma, cy)  
-%     btm = sigma.*cy;
-%     aprime = (-1+(top./btm)).^-1;
-    
 end
 function [ftip, froot, ftotal] = PrandtlTipRootCorrection(prop, SectionRadius, oper, a)
     r_R_Section = SectionRadius/prop.R; %non-dimensional
     F1 = (-prop.Nblades/2)*((1-r_R_Section)/r_R_Section)*sqrt(1+(oper.TSR*r_R_Section/(1-a))^2);
     F2 = (-prop.Nblades/2)*((r_R_Section-prop.blade_root)/r_R_Section)*sqrt(1+(oper.TSR*r_R_Section/(1-a))^2);
     
-%     F1 = (-prop.Nblades/2)*((1-r_R_Section)/r_R_Section)*(1/abs(sin(InflowAngle)));(prop, SectionRadius, InflowAngle
-%     F2 = (-prop.Nblades/2)*((r_R_Section-prop.blade_root)/prop.blade_root)*(1/abs(sin(InflowAngle)));
+%     F1 = (-prop.Nblades/2)*((1-r_R_Section)/r_R_Section)*(1/sin(InflowAngle));
+%     F2 = (-prop.Nblades/2)*((r_R_Section-prop.blade_root)/r_R_Section)*(1/sin(InflowAngle));
     
     ftip = (2/pi)*acos(exp(F1));    
     if (isnan(ftip))
@@ -98,15 +71,16 @@ function [ftip, froot, ftotal] = PrandtlTipRootCorrection(prop, SectionRadius, o
     end
     
     ftotal = ftip*froot;
-    ftotal(ftotal < 0.0001)= 0.0001;
-    
+    if (ftotal < 0.0001)
+        ftotal = 0.0001;
+    end
 end
-function [f_axial, f_tangential, gamma, alpha, U_resultant, cx,cy] = SectionLoads(U_axial, U_tangential, prop, polar, air)
+function [f_axial, f_tangential, gamma, alpha, U_resultant] = SectionLoads(U_axial, U_tangential, prop, polar, air)
     InflowAngle = atan2(U_axial, U_tangential); % [rad]
     alpha = rad2deg(InflowAngle) - prop.sectionpitch; 
     U_resultant = norm([U_axial, U_tangential]);
-    cl = interp1(polar.alpha, polar.Cl, alpha, 'linear', 'extrap');
-    cd = interp1(polar.alpha, polar.Cd, alpha, 'linear', 'extrap');
+    cl = interp1(polar.alpha, polar.Cl, alpha);
+    cd = interp1(polar.alpha, polar.Cd, alpha);
 %     lift = 0.5*air.density*(U_resultant^2)*(prop.sectionchord*prop.R)*cl;
 %     drag = 0.5*air.density*(U_resultant^2)*(prop.sectionchord*prop.R)*cd;
     
@@ -114,8 +88,6 @@ function [f_axial, f_tangential, gamma, alpha, U_resultant, cx,cy] = SectionLoad
     drag = 0.5*(U_resultant^2)*(prop.sectionchord*prop.R)*cd;
     f_axial = lift*cos(InflowAngle)+drag*sin(InflowAngle);
     f_tangential = lift*sin(InflowAngle)-drag*cos(InflowAngle);
-    cx = cl*cos(InflowAngle)+cd*sin(InflowAngle);
-    cy = cl*sin(InflowAngle)-cd*cos(InflowAngle);
 %     gamma = lift/(air.density*U_resultant);      
     gamma = lift/(U_resultant);      
 
@@ -125,8 +97,7 @@ function Results = SolveSection(index, polar, prop, air, oper)
     r_R2=prop.r_R(index+1);
     SectionArea = pi*(((r_R2*prop.R)^2)-((r_R1*prop.R)^2)); %[m^2]
     SectionRadius = 0.5*(r_R1+r_R2)*prop.R;%average radius [m]
-    SectionRotorSolidity = (prop.Nblades*prop.sectionchord*prop.R)/(2*pi*SectionRadius);
-
+    
     a = 0.1;%axial induction factor
     aprime = 0.1;%tangential induction factor
     
@@ -136,33 +107,24 @@ function Results = SolveSection(index, polar, prop, air, oper)
     for i=1:N
         U_axial = oper.U_inf*(1-a);
         U_tangential = oper.omega*SectionRadius*(1+aprime);       
-        [f_axial, f_tangential, gamma, alpha, InflowAngle, ~, ~]=SectionLoads(U_axial, U_tangential,prop, polar, air);
+        [f_axial, f_tangential, gamma, alpha, InflowAngle]=SectionLoads(U_axial, U_tangential,prop, polar, air);
         %Thrust coefficient at streamtube        
         load3D_axial = f_axial*prop.Nblades*(prop.dr(index)*prop.R);
 %         CT_streamtube= load3D_axial./(0.5*air.density*(oper.U_inf^2)*SectionArea);
         CT_streamtube= load3D_axial./(0.5*(oper.U_inf^2)*SectionArea);
-        
-        %%%%%%
         a_new=AxialInductionFactor(CT_streamtube);
         
-        [~, ~, Prandtl]=PrandtlTipRootCorrection(prop, SectionRadius, oper, a);
+        [~, ~, Prandtl]=PrandtlTipRootCorrection(prop, SectionRadius, oper, a_new);
         a_new = a_new/Prandtl;
         a = 0.75*a+0.25*a_new;%update new value of axial induction via weighted average
         
         aprime = AzimInductionFactor(f_tangential, prop, SectionRadius, oper, a, air);
         aprime = aprime/Prandtl;
-        %%%%%%
-%         [~, ~, Prandtl]=PrandtlTipRootCorrection(prop, SectionRadius, InflowAngle);
-%         a_new = AxialInductionFactor(CT_streamtube, Prandtl, SectionRotorSolidity, cx, InflowAngle);
-%         a = 0.75*a+0.25*a_new;%update new value of axial induction via weighted average
-%         aprime = AzimInductionFactor(Prandtl, InflowAngle, SectionRotorSolidity, cy); 
-%         
-        
         
         if (abs(a-a_new)<epsilon)
             break;
         end       
     end 
     
-    Results = [SectionRadius/prop.R, a, aprime, f_axial, f_tangential,gamma, InflowAngle,  i];
+    Results = [SectionRadius/prop.R, a, aprime, f_axial, f_tangential,gamma, alpha,  i];
 end
